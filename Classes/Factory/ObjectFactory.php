e22ab8b175e17011cd76f9f746236d1632e21c05
Claus Due <claus@namelesscoder.net>
2014-08-20 16:14:05 +0200

    [TASK] Object Factory first implementation
    Summary:
    Initial implementation of an extension-native
    ObjectFactory to load objects. This first implementation
    uses Extbase's ObjectManager to load objects and returns
    a dummy MasterConfiguration which will only return
    default values.
    
    Intention is to allow this ObjectFactory to create other
    class names based on registered implementations without
    depending on a TYPO3CMS or Flow3 specific method of
    doing so. It also allows custom initialization of some
    classes again without depending on a framework-specific
    strategy - and makes a central point for adapting the
    class loading, by letting the framework load another
    custom-purpose ObjectFactory.
    
    While it is not the main goal of this pattern, it also
    helps unit test writing in that the factory can be mocked
    to return other mocks with proper expectations.
    
    Reviewers: dkd-egerer, #forgetit-dev
    
    Reviewed By: dkd-egerer, #forgetit-dev
    
    Differential Revision: https://phabricator.dkd.de/D153

--- Classes/Factory/ObjectFactory.php
+++ Classes/Factory/ObjectFactory.php
@@ -1,6 +1,63 @@
 <?php
 namespace Dkd\CmisService\Factory;
 
+use Dkd\CmisService\Configuration\Definitions\MasterConfiguration;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\ObjectManagerInterface;
+
+/**
+ * Object Factory
+ *
+ * Wrapper for making instances of objects with constructor
+ * arguments in a fashion that supports the host system's
+ * object loader - in this default implementation using
+ * Extbase's ObjectManager to create object instances.
+ *
+ * @package Dkd\CmisService\Factory
+ */
 class ObjectFactory {
+
+	/**
+	 * Make an instance of $className, if any additional parameters
+	 * are present they will be used as constructor arguments.
+	 *
+	 * @param string $className
+	 * @return mixed
+	 */
+	public function makeInstance($className) {
+		/** @var ObjectManagerInterface $manager */
+		$manager = GeneralUtility::makeInstance('TYPO3\CMS\Extbase\Object\ObjectManager');
+		$arguments = func_get_args();
+		$instance = call_user_func_array(array($manager, 'get'), $arguments);
+		return $instance;
+	}
+
+	/**
+	 * Get the Master Configuration Definition which contains
+	 * API methods to query every other configuration option.
+	 *
+	 * TYPO3 CMS specific information:
+	 *
+	 * In order to correctly bootstrap the configuration which is
+	 * used for essential business logic of this package, the
+	 * TypoScript information is read - but only key variables
+	 * from _this_ reading of the TypoScript are used, namely
+	 * the _configuration Reader and Writer and associated
+	 * parameters_.
+	 *
+	 * If so configured, another configuration reader may read
+	 * the TypoScript again, this time putting it into the API
+	 * of the ConfigurationDefinition implementations. In the
+	 * standard configuration this is the default approach, but
+	 * other configuration may choose to use a static YAML file
+	 * as only configuration source, ignoring any TypoScript
+	 * except for these two key parameters for Reader and Writer.
+	 *
+	 * @return MasterConfiguration
+	 */
+	public function getConfiguration() {
+		$configuration = new MasterConfiguration();
+		return $configuration;
+	}
 
 }
--- Tests/Unit/Factory/ObjectFactoryTest.php
+++ Tests/Unit/Factory/ObjectFactoryTest.php
@@ -1,6 +1,25 @@
 <?php
 namespace Dkd\CmisService\Factory;
 
-class ObjectFactoryTest {
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+
+/**
+ * Class ObjectFactoryTest
+ *
+ * @package Dkd\CmisService\Factory
+ */
+class ObjectFactoryTest extends UnitTestCase {
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function getConfigurationReturnsInstanceOfMasterConfiguration() {
+		$factory = new ObjectFactory();
+		$configuration = $factory->getConfiguration();
+		$this->assertInstanceOf('Dkd\CmisService\Configuration\Definitions\MasterConfiguration', $configuration);
+	}
 
 }
