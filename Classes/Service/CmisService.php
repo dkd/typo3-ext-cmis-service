5be5745f12698249bdd1813e72ba727f41dc426e
Claus Due <claus@namelesscoder.net>
2014-08-27 17:00:06 +0200

    [TASK] Add main CMIS API: CmisService
    Summary:
    Service class skeleton to contain master API methods
    for other code to interact with CMIS.
    
    Reviewers: dkd-egerer, #forgetit-dev
    
    Reviewed By: dkd-egerer, #forgetit-dev
    
    Differential Revision: https://phabricator.dkd.de/D181

--- Classes/Service/CmisService.php
+++ Classes/Service/CmisService.php
@@ -3,6 +3,14 @@ namespace Dkd\CmisService\Service;
 
 use Dkd\CmisService\SingletonInterface;
 
+/**
+ * CMIS Service
+ *
+ * Contains the master API for other code to interact
+ * or acquire other interaction APIs for CMIS.
+ *
+ * @package Dkd\CmisService\Service
+ */
 class CmisService implements SingletonInterface {
 
 }
--- Tests/Unit/Service/CmisServiceTest.php
+++ Tests/Unit/Service/CmisServiceTest.php
@@ -3,6 +3,21 @@ namespace Dkd\CmisService\Service;
 
 use TYPO3\CMS\Core\Tests\UnitTestCase;
 
+/**
+ * Class CmisServiceTest
+ *
+ * @package Dkd\CmisService\Service
+ */
 class CmisServiceTest extends UnitTestCase {
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function placeholder() {
+		$this->markTestIncomplete('Test not yet implemented');
+	}
 
 }
