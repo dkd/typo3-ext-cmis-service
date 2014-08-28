8bf0968bd5d491aee11f3c5faf8f49860c56a6cb
Claus Due <claus@namelesscoder.net>
2014-08-28 12:41:01 +0200

    [TASK] Add interface for Queue implementations
    Reviewers: #forgetit-dev, dkd-egerer
    
    Reviewed By: #forgetit-dev, dkd-egerer
    
    Differential Revision: https://phabricator.dkd.de/D183

---
+++ Classes/Queue/QueueCachableInterface.php
@@ -0,0 +1,23 @@
+<?php
+namespace Dkd\CmisService\Queue;
+
+use Dkd\CmisService\Cache\VariableFrontendInterface;
+
+/**
+ * Queue Cachable Interface
+ *
+ * Must be implemented if the Queue implementation also
+ * should receive an instance of a VariableFrontend to
+ * implement caching-based features.
+ *
+ * @package Dkd\CmisService\Queue
+ */
+interface QueueCachableInterface {
+
+	/**
+	 * @param VariableFrontendInterface $frontend
+	 * @return void
+	 */
+	public function setCache(VariableFrontendInterface $frontend);
+
+}
--- Classes/Queue/QueueInterface.php
+++ Classes/Queue/QueueInterface.php
@@ -1,6 +1,42 @@
 <?php
 namespace Dkd\CmisService\Queue;
 
+use Dkd\CmisService\Task\TaskInterface;
+
+/**
+ * Queue Interface
+ *
+ * Must be implemented by system-configured Queue class(es).
+ *
+ * @package Dkd\CmisService\Queue
+ */
 interface QueueInterface {
+
+	/**
+	 * @param TaskInterface[] $task
+	 * @return void
+	 */
+	public function addAll(array $tasks);
+
+	/**
+	 * @param TaskInterface $task
+	 * @return void
+	 */
+	public function add(TaskInterface $task);
+
+	/**
+	 * @return TaskInterface
+	 */
+	public function pick();
+
+	/**
+	 * @return void
+	 */
+	public function save();
+
+	/**
+	 * @return void
+	 */
+	public function load();
 
 }
