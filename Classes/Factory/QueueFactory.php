53a02fe5cb135a7f53f58a0c20a62d160c187971
Claus Due <claus@namelesscoder.net>
2014-09-01 16:26:43 +0200

    [FEATURE] Implement QueueFactory
    Reviewers: dkd-egerer, #forgetit-dev
    
    Reviewed By: dkd-egerer, #forgetit-dev
    
    Differential Revision: https://phabricator.dkd.de/D201

--- Classes/Factory/QueueFactory.php
+++ Classes/Factory/QueueFactory.php
@@ -1,6 +1,66 @@
 <?php
 namespace Dkd\CmisService\Factory;
 
+use Dkd\CmisService\Queue\QueueInterface;
+use Dkd\CmisService\Queue\SimpleQueue;
+
+/**
+ * Queue Factory
+ *
+ * Fetches an instance of the Queue implementation the
+ * host system has configured as active, preparing it
+ * for use.
+ *
+ * @package Dkd\CmisService\Factory
+ */
 class QueueFactory {
+
+	const DEFAULT_QUEUE_CLASS = 'Dkd\\CmisService\\Queue\\SimpleQueue';
+
+	/**
+	 * @var QueueInterface
+	 */
+	protected static $instance;
+
+	/**
+	 * Fetches the Queue implementation configured as
+	 * active by the host system. Queue instance is
+	 * treated as single instance and instance is
+	 * reused on subsequent fetches.
+	 *
+	 * @return QueueInterface
+	 */
+	public function fetchQueue() {
+		if (TRUE === self::$instance instanceof QueueInterface) {
+			return self::$instance;
+		}
+		return $this->initializeQueue();
+	}
+
+	/**
+	 * Initialize a Queue implementation and return it.
+	 *
+	 * @return QueueInterface
+	 */
+	protected function initializeQueue() {
+		$className = $this->getConfiguredQueueClassName();
+		$objectFactory = new ObjectFactory();
+		$cacheFactory = new CacheFactory();
+		$queueFactory = new QueueFactory();
+		$queueCache = $cacheFactory->fetchCache(SimpleQueue::CACHE_IDENTITY);
+		self::$instance = new $className();
+		self::$instance->setCache($queueCache);
+		return self::$instance;
+	}
+
+	/**
+	 * Gets the currently configured Queue implementation class
+	 * name or, if none is defined, the default implementation.
+	 *
+	 * @return string
+	 */
+	protected function getConfiguredQueueClassName() {
+		return self::DEFAULT_QUEUE_CLASS;
+	}
 
 }
--- Tests/Unit/Factory/QueueFactoryTest.php
+++ Tests/Unit/Factory/QueueFactoryTest.php
@@ -1,6 +1,83 @@
 <?php
 namespace Dkd\CmisService\Factory;
 
-class QueueFactoryTest {
+use TYPO3\CMS\Core\Core\Bootstrap;
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+
+/**
+ * Class QueueFactoryTest
+ *
+ * @package Dkd\CmisService\Factory
+ */
+class QueueFactoryTest extends UnitTestCase {
+
+	/**
+	 * Setup
+	 *
+	 * @return void
+	 */
+	protected function setUp() {
+		if (FALSE === defined('TYPO3_version')) {
+			Bootstrap::getInstance()->baseSetup('vendor/typo3/cms/typo3');
+		}
+		parent::setUp();
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function getConfiguredQueueClassNameReturnsValidClassName() {
+		$factory = new QueueFactory();
+		$className = $this->callInaccessibleMethod($factory, 'getConfiguredQueueClassName');
+		$this->assertTrue(class_exists($className));
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function initializeQueueReturnsValidClassInstance() {
+		$factory = new QueueFactory();
+		$instance = $this->callInaccessibleMethod($factory, 'initializeQueue');
+		$this->assertInstanceOf('Dkd\CmisService\Queue\QueueInterface', $instance);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function fetchQueueCallsExpectedMethodsAndReturnsValidClassInstance() {
+		$expectedQueueClassName = QueueFactory::DEFAULT_QUEUE_CLASS;
+		$expectedQueueClassInstance = new $expectedQueueClassName();
+		$factory = $this->getAccessibleMock('Dkd\CmisService\Factory\QueueFactory', array('initializeQueue'));
+		$factory->_setStatic('instance', NULL);
+		$factory->expects($this->once())->method('initializeQueue')->will($this->returnValue($expectedQueueClassInstance));
+		$queue = $factory->fetchQueue();
+		$this->assertInstanceOf($expectedQueueClassName, $queue);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function fetchQueueReturnsPreparedInstanceOnSubsequentUseIfInternalInstanceIsSet() {
+		$expectedQueueClassName = QueueFactory::DEFAULT_QUEUE_CLASS;
+		$expectedQueueClassInstance = new $expectedQueueClassName();
+		$factory = $this->getAccessibleMock('Dkd\CmisService\Factory\QueueFactory', array('initializeQueue'));
+		$factory->_setStatic('instance', $expectedQueueClassInstance);
+		$factory->expects($this->never())->method('initializeQueue');
+		$queue = $factory->fetchQueue();
+		$this->assertInstanceOf($expectedQueueClassName, $queue);
+		$this->assertSame($expectedQueueClassInstance, $queue);
+	}
 
 }
