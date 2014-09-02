a0b9a8ca06fe6b544c5cb2f6379281f87fb4a506
Claus Due <claus@namelesscoder.net>
2014-09-02 15:09:28 +0200

    [TASK] Add basic test case for worker
    Reviewers: #forgetit-dev, dkd-egerer
    
    Reviewed By: #forgetit-dev, dkd-egerer
    
    Differential Revision: https://phabricator.dkd.de/D194

--- Tests/Unit/Queue/SimpleWorkerTest.php
+++ Tests/Unit/Queue/SimpleWorkerTest.php
@@ -1,8 +1,31 @@
 <?php
 namespace Dkd\CmisService\Queue;
 
+use Dkd\CmisService\Execution\Result;
+use Dkd\CmisService\Tests\Fixtures\Task\DummyTask;
 use TYPO3\CMS\Core\Tests\UnitTestCase;
 
+/**
+ * Class SimpleWorkerTest
+ *
+ * @package Dkd\CmisService\Queue
+ */
 class SimpleWorkerTest extends UnitTestCase {
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function executeTaskReturnsExecutionResultObject() {
+		$result = new Result();
+		$task = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Task\\DummyTask', array('resolveExecutionObject'));
+		$execution = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Execution\\DummyExecution', array('execute'));
+		$task->expects($this->once())->method('resolveExecutionObject')->will($this->returnValue($execution));
+		$execution->expects($this->once())->method('execute')->with($task)->will($this->returnValue($result));
+		$worker = new SimpleWorker();
+		$worker->execute($task);
+	}
 
 }
