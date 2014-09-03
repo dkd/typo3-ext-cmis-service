3531a16fc28e83b8f3e1366037923b1fdcbc49b9
Claus Due <claus@namelesscoder.net>
2014-09-03 15:18:49 +0200

    [FEATURE] Implement ConfigurationManager logic
    Summary:
    Implements logic for using the Reader and Writer patterns through
    a common API.
    
    Reviewers: #forgetit-dev, dkd-egerer
    
    Reviewed By: #forgetit-dev, dkd-egerer
    
    Differential Revision: https://phabricator.dkd.de/D197

--- Classes/Configuration/ConfigurationManager.php
+++ Classes/Configuration/ConfigurationManager.php
@@ -1,30 +1,73 @@
 <?php
 namespace Dkd\CmisService\Configuration;
 
+use Dkd\CmisService\Configuration\Definitions\MasterConfiguration;
+use Dkd\CmisService\Configuration\Reader\ConfigurationReaderInterface;
+use Dkd\CmisService\Configuration\Writer\ConfigurationWriterInterface;
+use Dkd\CmisService\SingletonInterface;
+use TYPO3\CMS\Core\Log\Writer\WriterInterface;
+
 /**
  * Configuration Manager
  *
  * Handles reading and writing of configuration.
  *
- * Configuration writing is, if enabled, implemented
- * as a pseudo-cache; writing to the configured Writer
- * if the configuration resource identity is different
- * from the one used by the Reader. This results in a
- * behavior where configuration can be read from, for
- * example, a database table or XML file and stored as
- * a YAML file. The YAML file is then used as source
- * only if it exists, making it behave like a cached
- * representation of whichever configuration is live.
+ * Allows three different levels of operation:
  *
- * In other words: the Configuration Manager supports
- * a total of TWO potentially different Reader and
- * Writer resource identifiers and providing a different
- * resource identifier to the Writer also makes the
- * Reader use that resource if it exists.
+ * 1) Pure Reader
+ *
+ * When the constructor is passed only a Reader
+ * instance - which is required - the Manager will
+ * behave in a read-only fashion, never dispatching
+ * calls to the Writer or touching the cache.
+ *
+ * 2) Reader and Writer
+ *
+ * When given a Reader and a Writer, the Manager
+ * operates by always reading from the Reader and
+ * saving to the Writer as determined by a system
+ * configuration parameter that must be enabled for
+ * writing to happen - and a simple checksum to see if
+ * the destination actually requires a rewrite. In this
+ * mode the cache is never touched.
+ *
+ * 3) Reader, Writer and Cache
+ *
+ * When given all three Reader, Writer and another
+ * Reader instance, the second Reader instance will
+ * be used as a sort of cache; reading the definitions
+ * from that source if it exists, otherwise defaulting
+ * to the original Reader - resulting in this behavior:
+ *
+ *    - If $cache Reader exists and has a source that
+ *      also exists, the source is loaded from $cache.
+ *    - If $cache Reader exists but has no existing
+ *      source, $reader Reader is used to fetch the
+ *      definitions from storage.
+ *    - If $writer Writer exists and has a target which
+ *      has a checksum different than current definition,
+ *      $writer is told to write target on lifecycle end.
+ *
+ * Note: since this behavior is governed by system settings
+ * it is possible to let the Production environment not
+ * use any caching or writing but simply read from a small
+ * static YAML file with L1 caching only, for optimal speed.
+ *
+ * Note: since this class is a Singleton, constructor
+ * parameters are only respected when the first instance is
+ * created and ignored for all others.
  *
  * @package Dkd\CmisService\Configuration
  */
 class ConfigurationManager {
+
+	const CACHE_RESOURCE = 'typo3temp/Cache/Code/cmis-service-cache.yaml';
+	const MASTER_RESOURCE = 'plugin.tx_cmisservice.settings';
+
+	/**
+	 * @var ConfigurationReaderInterface
+	 */
+	protected $cache;
 
 	/**
 	 * @var ConfigurationReaderInterface
@@ -35,5 +78,126 @@ class ConfigurationManager {
 	 * @var ConfigurationWriterInterface
 	 */
 	protected $writer;
+
+	/**
+	 * L1 cache for active MasterConfiguration
+	 *
+	 * @var MasterConfiguration
+	 */
+	protected $masterConfiguration;
+
+	/**
+	 * Constructor method. See class doc comment for usage.
+	 *
+	 * @param ConfigurationReaderInterface $reader
+	 * @param ConfigurationWriterInterface $writer
+	 * @param ConfigurationReaderInterface $cache
+	 */
+	public function __construct(
+			ConfigurationReaderInterface $reader,
+			ConfigurationWriterInterface $writer = NULL,
+			ConfigurationReaderInterface $cache = NULL
+	) {
+		$this->reader = $reader;
+		$this->writer = $writer;
+		$this->cache = $cache;
+	}
+
+	/**
+	 * Object lifetime termination.
+	 *
+	 * @return void
+	 */
+	public function __destruct() {
+		$this->createOrUpdateCachedDefinition();
+	}
+
+	/**
+	 * Gets (with on-the-fly loading) the active MasterConfiguration
+	 * definition used by the system.
+	 *
+	 * @return MasterConfiguration
+	 * @api
+	 */
+	public function getMasterConfiguration() {
+		if (TRUE === $this->masterConfiguration instanceof MasterConfiguration) {
+			return $this->masterConfiguration;
+		}
+		if (TRUE === $this->cache instanceof ConfigurationReaderInterface) {
+			return $this->masterConfiguration = $this->cache->read(self::CACHE_RESOURCE);
+		}
+		return $this->masterConfiguration = $this->reader->read(self::MASTER_RESOURCE);
+	}
+
+	/**
+	 * Export the loaded MasterConfiguration to the target identified
+	 * in the method parameter, using the ConfigurationWriter
+	 * implementation currently configured. If no Writer is configured
+	 * this method throws a RuntimeException.
+	 *
+	 * @param mixed $targetResourceIdentifier
+	 * @return boolean
+	 * @throws \RuntimeException
+	 * @api
+	 */
+	public function export($targetResourceIdentifier) {
+		if (FALSE === $this->writer instanceof ConfigurationWriterInterface) {
+			throw new \RuntimeException('Cannot export configuration - no ConfigurationWriter configured', 1409181458);
+		}
+		$definition = $this->getMasterConfiguration();
+		return $this->writer->write($definition, $targetResourceIdentifier);
+	}
+
+	/**
+	 * Expires (marks for rebuilding by deleting) the currently cached
+	 * definition. Does nothing if no cached definition is configured.
+	 * Warning: not public API! The only legitimate use is when delegated
+	 * from listeners reacting to cache truncation in the framework.
+	 *
+	 * @return NULL
+	 */
+	public function expireCachedDefinition() {
+		if (FALSE === $this->cache instanceof ConfigurationReaderInterface) {
+			return NULL;
+		}
+		$this->removeResource(self::CACHE_RESOURCE);
+		return NULL;
+	}
+
+	/**
+	 * Creates or updates (if required) the cached representation.
+	 * Returns NULL if no operation was performed. Returns TRUE if
+	 * cached definition was updated successfully. Returns FALSE if
+	 * some (silent) error occurred during writing, or if writing
+	 * was skipped by the Writer but not due to checksum mismatch.
+	 * On FALSE, further errors will have been logged.
+	 *
+	 * @return boolean|NULL
+	 */
+	protected function createOrUpdateCachedDefinition() {
+		if (FALSE === $this->writer instanceof ConfigurationWriterInterface) {
+			return NULL;
+		}
+		if (FALSE === $this->cache instanceof ConfigurationReaderInterface) {
+			return NULL;
+		}
+		$currentChecksum = $this->reader->checksum(self::MASTER_RESOURCE);
+		$cachedChecksum = $this->cache->checksum(self::CACHE_RESOURCE);
+		if ($cachedChecksum !== $currentChecksum) {
+			return $this->writer->write($this->getMasterConfiguration(), self::CACHE_RESOURCE);
+		}
+		return NULL;
+	}
+
+	/**
+	 * Removes a resource if it exists. Returns TRUE if the file
+	 * was removed or if it did not already exist.
+	 *
+	 * @param string $file
+	 * @return boolean
+	 */
+	protected function removeResource($resourceIdentifier) {
+		return file_exists($resourceIdentifier) ? unlink($resourceIdentifier) : TRUE;
+	}
 
 }
--- Tests/Unit/Configuration/ConfigurationManagerTest.php
+++ Tests/Unit/Configuration/ConfigurationManagerTest.php
@@ -1,7 +1,12 @@
 <?php
 namespace Dkd\CmisService\Configuration;
 
+use Dkd\CmisService\Tests\Fixtures\Configuration\DummyMasterConfiguration;
+use Dkd\CmisService\Tests\Fixtures\Configuration\DummyReader;
+use Dkd\CmisService\Tests\Fixtures\Configuration\DummyWriter;
+use TYPO3\CMS\Core\Core\Bootstrap;
 use TYPO3\CMS\Core\Tests\UnitTestCase;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Class ConfigurationManagerTest
@@ -11,14 +16,311 @@ use TYPO3\CMS\Core\Tests\UnitTestCase;
 class ConfigurationManagerTest extends UnitTestCase {
 
 	/**
+	 * Setup
+	 *
+	 * Checks the very first constant defined by the TYPO3 CMS
+	 * initialization "Bootstrap" class. If the constant is not
+	 * defined we assume a minimal environment must be created:
+	 *
+	 * - to define constants some TYPO3 CMS classes need
+	 * - to initialize the TYPO3 CMS class loader _caches_
+	 * - to unregister the actual class loader from TYPO3 CMS,
+	 *   falling back to the Composer autoloading.
+	 *
+	 * The whole thing is initialized by setting PATH_thisScript
+	 * since this constant is the one TYPO3's Bootstrap uses
+	 * when detecting the starting point for other path constants.
+	 *
+	 * @return void
+	 */
+	protected function setUp() {
+		if (FALSE === defined('TYPO3_version')) {
+			define('PATH_thisScript', realpath('vendor/typo3/cms/typo3/index.php'));
+			$bootstrap = Bootstrap::getInstance()->baseSetup('typo3/')->initializeClassLoader()
+				->unregisterClassLoader();
+		}
+		parent::setUp();
+	}
+
+	/**
 	 * Unit test
 	 *
 	 * @test
 	 * @return void
 	 */
-	public function supportsInstanciationUsingNewKeyword() {
-		$instance = new ConfigurationManager();
-		$this->assertInstanceOf('Dkd\CmisService\Configuration\ConfigurationManager', $instance);
+	public function constructorSetsInternalPropertiesGivenOneArgument() {
+		$dummyReader = new DummyReader();
+		$manager = new ConfigurationManager($dummyReader);
+		$this->assertAttributeSame($dummyReader, 'reader', $manager);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function constructorSetsInternalPropertiesGivenTwoArguments() {
+		$dummyReader = new DummyReader();
+		$dummyWriter = new DummyWriter();
+		$manager = new ConfigurationManager($dummyReader, $dummyWriter);
+		$this->assertAttributeSame($dummyReader, 'reader', $manager);
+		$this->assertAttributeSame($dummyWriter, 'writer', $manager);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function constructorSetsInternalPropertiesGivenThreeArguments() {
+		$dummyReader = new DummyReader();
+		$dummyWriter = new DummyWriter();
+		$dummyCache = new DummyReader();
+		$manager = new ConfigurationManager($dummyReader, $dummyWriter, $dummyCache);
+		$this->assertAttributeSame($dummyReader, 'reader', $manager);
+		$this->assertAttributeSame($dummyWriter, 'writer', $manager);
+		$this->assertAttributeSame($dummyCache, 'cache', $manager);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function getMasterConfigurationReturnsLevelOneCacheIfSet() {
+		$dummyWriter = new DummyWriter();
+		$mockReader = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyReader', array('checksum'));
+		$mockCache = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyReader', array('checksum'));
+		$dummyConfiguration = new DummyMasterConfiguration();
+		$manager = $this->getAccessibleMock(
+			'Dkd\\CmisService\\Configuration\\ConfigurationManager',
+			array('dummy'),
+			array('reader' => $mockReader, 'writer' => $dummyWriter, 'cache' => $mockCache)
+		);
+		$mockCache->expects($this->never())->method('read');
+		$mockReader->expects($this->never())->method('read');
+		$manager->_set('masterConfiguration', $dummyConfiguration);
+		$this->assertSame($dummyConfiguration, $manager->getMasterConfiguration());
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function getMasterConfigurationReturnsCacheReadIfCacheSet() {
+		$dummyWriter = new DummyWriter();
+		$mockReader = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyReader', array('checksum'));
+		$mockCache = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyReader', array('checksum', 'read'));
+		$dummyConfiguration = new DummyMasterConfiguration();
+		$manager = new ConfigurationManager($mockReader, $dummyWriter, $mockCache);
+		$mockCache->expects($this->once())->method('read')
+			->with(ConfigurationManager::CACHE_RESOURCE)
+			->will($this->returnValue($dummyConfiguration));
+		$mockReader->expects($this->never())->method('read');
+		$this->assertSame($dummyConfiguration, $manager->getMasterConfiguration());
+		$this->assertAttributeSame($dummyConfiguration, 'masterConfiguration', $manager);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function getMasterConfigurationReturnsReaderReadAndSetsLevelOneCacheIfCacheNotSet() {
+		$mockReader = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyReader', array('checksum', 'read'));
+		$mockWriter = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyWriter', array('checksum'));
+		$mockConfiguration = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyMasterConfiguration');
+		$mockReader->expects($this->once())->method('read')
+			->with(ConfigurationManager::MASTER_RESOURCE)
+			->will($this->returnValue($mockConfiguration));
+		$manager = new ConfigurationManager($mockReader, $mockWriter);
+		$result = $manager->getMasterConfiguration();
+		$this->assertSame($mockConfiguration, $result);
+		$this->assertAttributeSame($mockConfiguration, 'masterConfiguration', $manager);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function exportThrowsExpectedRuntimeExceptionIfNoWriterSet() {
+		$this->setExpectedException('RuntimeException', NULL, 1409181458);
+		$mockReader = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyReader', array('checksum'));
+		$manager = new ConfigurationManager($mockReader);
+		$manager->export('voidResourceIdentifier');
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function exportCallsExpectedMethodSequenceWithExpectedParameters() {
+		$mockConfiguration = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyMasterConfiguration');
+		$mockReader = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyReader', array('checksum'));
+		$mockWriter = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyWriter', array('checksum', 'write'));
+		$mockWriter->expects($this->once())->method('write')
+			->with($mockConfiguration, 'voidResourceIdentifier')
+			->will($this->returnValue(TRUE));
+		$manager = $this->getMock(
+			'Dkd\\CmisService\\Configuration\\ConfigurationManager',
+			array('getMasterConfiguration'),
+			array($mockReader, $mockWriter)
+		);
+		$manager->expects($this->once())->method('getMasterConfiguration')->will($this->returnValue($mockConfiguration));
+		$result = $manager->export('voidResourceIdentifier');
+		$this->assertTrue($result);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function expireCachedDefinitionReturnsNullIfNoCacheSet() {
+		$mockReader = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyReader', array('checksum'));
+		$manager = $this->getMock(
+			'Dkd\\CmisService\\Configuration\\ConfigurationManager',
+			array('removeResource'),
+			array($mockReader)
+		);
+		$manager->expects($this->never())->method('removeResource');
+		$result = $manager->expireCachedDefinition();
+		$this->assertNull($result);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function expireCachedDefinitionRemovesFileAndReturnsNullIfCacheSet() {
+		$mockReader = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyReader', array('checksum'));
+		$mockWriter = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyWriter', array('checksum'));
+		$mockCache = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyReader', array('checksum'));
+		$manager = $this->getMock(
+			'Dkd\\CmisService\\Configuration\\ConfigurationManager',
+			array('removeResource'),
+			array($mockReader, $mockWriter, $mockCache)
+		);
+		$manager->expects($this->once())->method('removeResource')
+			->with(ConfigurationManager::CACHE_RESOURCE)
+			->will($this->returnValue(TRUE));
+		$result = $manager->expireCachedDefinition();
+		$this->assertNull($result);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function createOrUpdateCachedDefinitionReturnsEarlyNullIfNoWriterSet() {
+		$mockReader = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyReader', array('checksum'));
+		$mockReader->expects($this->never())->method('checksum');
+		$manager = new ConfigurationManager($mockReader);
+		$result = $this->callInaccessibleMethod($manager, 'createOrUpdateCachedDefinition');
+		$this->assertNull($result);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function createOrUpdateCachedDefinitionReturnsEarlyNullIfNoCacheSet() {
+		$mockReader = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyReader', array('checksum'));
+		$mockWriter = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyWriter', array('checksum'));
+		$mockReader->expects($this->never())->method('checksum');
+		$manager = new ConfigurationManager($mockReader, $mockWriter);
+		$result = $this->callInaccessibleMethod($manager, 'createOrUpdateCachedDefinition');
+		$this->assertNull($result);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function createOrUpdateCachedDefinitionWritesCachedDefinitionAndReturnsTrueIfWriterAndCacheSetAndChecksumsDiffer() {
+		$mockReader = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyReader', array('checksum'));
+		$mockWriter = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyWriter', array('write'));
+		$mockCache = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyReader', array('checksum', 'read'));
+		$dummyConfiguration = new DummyMasterConfiguration();
+		$mockReader->expects($this->once())->method('checksum')
+			->will($this->returnValue('foobarchecksum'));
+		$mockCache->expects($this->once())->method('checksum')
+			->will($this->returnValue('differentchecksum'));
+		$mockCache->expects($this->never())->method('read')
+			->with(ConfigurationManager::CACHE_RESOURCE)
+			->will($this->returnValue($mockConfiguration));
+		$mockWriter->expects($this->once())->method('write')
+			->with($dummyConfiguration, ConfigurationManager::CACHE_RESOURCE)
+			->will($this->returnValue(TRUE));
+		$manager = $this->getMock(
+			'Dkd\\CmisService\\Configuration\\ConfigurationManager',
+			array('getMasterConfiguration'),
+			array($mockReader, $mockWriter, $mockCache)
+		);
+		$manager->expects($this->once())->method('getMasterConfiguration')->will($this->returnValue($dummyConfiguration));
+		$result = $this->callInaccessibleMethod($manager, 'createOrUpdateCachedDefinition');
+		$this->assertTrue($result);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function createOrUpdateCachedDefinitionSkipsWritingAndReturnsNullIfWriterAndCacheSetAndChecksumsSame() {
+		$mockReader = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyReader', array('checksum'));
+		$mockWriter = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyWriter', array('write'));
+		$mockCache = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyReader', array('checksum'));
+		$mockReader->expects($this->once())->method('checksum')->will($this->returnValue('foobarchecksum'));
+		$mockCache->expects($this->once())->method('checksum')->will($this->returnValue('foobarchecksum'));
+		$mockWriter->expects($this->never())->method('write');
+		$manager = $this->getMock(
+			'Dkd\\CmisService\\Configuration\\ConfigurationManager',
+			array('getMasterConfiguration'),
+			array($mockReader, $mockWriter, $mockCache)
+		);
+		$manager->expects($this->never())->method('getMasterConfiguration');
+		$result = $this->callInaccessibleMethod($manager, 'createOrUpdateCachedDefinition');
+		$this->assertNull($result);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function removeResourceUnlinksFile() {
+		$mockReader = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Configuration\\DummyReader', array('checksum'));
+		$manager = new ConfigurationManager($mockReader);
+		$typo3tempFile = GeneralUtility::getFileAbsFileName('typo3temp/tempfile');
+		$temporaryFile = TRUE === isset($GLOBALS['TYPO3_CONF_VARS']) ? $typo3tempFile : 'tempfile';
+		$temporaryFile .= uniqid();
+		touch($temporaryFile);
+		$this->assertFileExists($temporaryFile);
+		$result = $this->callInaccessibleMethod($manager, 'removeResource', $temporaryFile);
+		$this->assertTrue($result);
+		$this->assertFileNotExists($temporaryFile);
 	}
 
 }
