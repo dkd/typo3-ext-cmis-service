819c4f67af1831e45e58fd8367a46b353df07f22
Claus Due <claus@namelesscoder.net>
2014-09-03 15:54:22 +0200

    [FEATURE] Implement an initial SimpleQueue
    Summary:
    WORD OF WARNING:
    
    I am aware that this current implementation has some obvious technical
    flaws, mainly in that it uses a cache table to store the entire queue
    as one big object, inevitably causing problems on large queues and
    due to the nature of caches, gets cleared when "system" caches are
    cleared.
    
    The final SimpleQueue should use plain records in a custom table, but
    this first implementation allows work to continue until the other parts
    are better established and the queue implementation can be optimised.
    
    As such, it is WIP but I would still like to merge it at this point
    with the promise of a more solid implementation coming shortly after
    completing the CommandController / Scheduler parts that actually use
    this Queue.
    
    I'm of course open to discuss the final implementation of SimpleQueue
    that I plan to do based on a table without TCA. Also a possible topic
    would be whether or not we should implement one or two other Queue
    types - or just stick with a DB-based single implementation.
    
    Reviewers: dkd-egerer, #forgetit-dev
    
    Reviewed By: dkd-egerer, #forgetit-dev
    
    Differential Revision: https://phabricator.dkd.de/D200

--- Classes/Queue/SimpleQueue.php
+++ Classes/Queue/SimpleQueue.php
@@ -1,6 +1,214 @@
 <?php
 namespace Dkd\CmisService\Queue;
 
-class SimpleQueue implements QueueInterface {
+use Dkd\CmisService\Cache\VariableFrontendInterface;
+use Dkd\CmisService\Factory\WorkerFactory;
+use Dkd\CmisService\Task\TaskInterface;
+use TYPO3\CMS\Core\Locking\Locker;
+
+/**
+ * Simple Queue
+ * ============
+ *
+ * Implementation of a Queue based on a cached list of
+ * Tasks which will be serialized and implementing
+ * locking of write operations to preserve integrity.
+ *
+ * Any consumer can "pick" a Task which is then assigned
+ * a Worker instance and returned. The execution of the
+ * task is then handled in the consumer's context using
+ * an Execution instance.
+ *
+ * Once "picked" the Task is removed from the Queue.
+ *
+ * Usage example
+ * =============
+ *
+ * // ADDING TASKS (record indexing Task as example):
+ * $taskFactory = new TaskFactory();
+ * $queueFactory = new QueueFactory();
+ * $task = $taskFactory->createRecordIndexingTask();
+ * $task->setParameter('foobar', 'baz');
+ * $queue = $queueFactory->fetchQueue();
+ * $queue->add($task);
+ * // alternative: create array of Tasks and use addAll($tasks).
+ *
+ *
+ * // PICKING AND EXECUTING TASKS:
+ * $factory = new QueueFactory();
+ * $queue = $factory->fetchQueue();
+ * $task = $queue->pick();
+ *
+ * // dummy example execution without ANY handling.
+ * // $result is a Dkd\CmisService\Execution\Result
+ * $result = $task->getWorker()->execute($task);
+ *
+ * @package Dkd\CmisService\Queue
+ */
+class SimpleQueue implements QueueInterface, QueueCachableInterface {
+
+	// Constants from Locker::FOOBAR possible, empty string for sys default
+	const LOCK_METHOD = '';
+	const LOCK_ID = 'dkd.cmisservice.queueLock';
+	const MAX_LOCK_WAIT = 120;
+	const CACHE_IDENTITY = 'SimpleQueue';
+
+	/**
+	 * @var TaskInterface[]
+	 */
+	protected $queue = array();
+
+	/**
+	 * @var VariableFrontend
+	 */
+	protected $cache;
+
+	/**
+	 * @var Locker
+	 */
+	protected $locker;
+
+	/**
+	 * Prepare this instance with an instance of a "Cache" which
+	 * supports saving a serialized state of the internal data
+	 * storage to a persistent storage, by default a DB table.
+	 *
+	 * @api
+	 * @param VariableFrontendInterface $frontend
+	 * @return void
+	 */
+	public function setCache(VariableFrontendInterface $frontend) {
+		$this->cache = $frontend;
+	}
+
+	/**
+	 * Adds a Task to the Queue. Internally, locks the Queue
+	 * for write operation and stores the updated (cached)
+	 * value, then releases the lock.
+	 *
+	 * @api
+	 * @param TaskInterface $task
+	 * @return void
+	 */
+	public function add(TaskInterface $task) {
+		$id = $task->getId();
+		$this->lock();
+		$task->queue();
+		$this->queue[$id] = $task;
+		$this->save();
+		$this->release();
+	}
+
+	/**
+	 * Adds all Tasks in $tasks as a bulk operation, locking
+	 * the write operation before adding all Tasks to the
+	 * Queue and saving, then releasing.
+	 *
+	 * @api
+	 * @param TaskInterface[] $task
+	 * @return void
+	 */
+	public function addAll(array $tasks) {
+		$this->lock();
+		foreach ($tasks as $task) {
+			$id = $task->getId();
+			$task->queue();
+			$this->queue[$id] = $task;
+		}
+		$this->save();
+		$this->release();
+	}
+
+	/**
+	 * Picks (for execution) the first-added (FIFO principle)
+	 * Task from the Queue, removing it from the internal data
+	 * storage in the process. Assigns a Worker instance to
+	 * the Task which is then prepared for actual execution
+	 * via the Execution instance returned from the Task.
+	 *
+	 * @api
+	 * @return TaskInterface|NULL
+	 */
+	public function pick() {
+		if (0 === count($this->queue)) {
+			return NULL;
+		}
+		$this->lock();
+		/** @var TaskInterface $task */
+		$task = array_shift($this->queue);
+		$this->save();
+		$this->release();
+		$factory = new WorkerFactory();
+		$worker = $factory->createWorker();
+		$task->assign($worker);
+		return $task;
+	}
+
+	/**
+	 * Saves the current state of the Queue by delegating to
+	 * a "Cache" implementation, by default utilizing a DB
+	 * table storage in TYPO3 CMS.
+	 *
+	 * @api
+	 * @return void
+	 */
+	public function save() {
+		$this->lock();
+		$this->cache->set(self::CACHE_IDENTITY, $this->queue);
+		$this->release();
+	}
+
+	/**
+	 * Loads the cached state of the Queue into this instance's
+	 * internal storage. Subsequent calls will re-load the data
+	 * and overwrite what is currently loaded.
+	 *
+	 * @api
+	 * @return void
+	 */
+	public function load() {
+		if (TRUE === $this->cache->has(self::CACHE_IDENTITY)) {
+			$this->queue = $this->cache->get(self::CACHE_IDENTITY);
+		}
+	}
+
+	/**
+	 * Gets the Locker used by the Queue
+	 *
+	 * @return Locker
+	 */
+	protected function getLocker() {
+		if (FALSE === $this->locker instanceof Locker) {
+			$this->locker = new Locker(self::LOCK_ID, self::LOCK_METHOD, self::MAX_LOCK_WAIT);
+		}
+		return $this->locker;
+	}
+
+	/**
+	 * Lock the queue, protecting it from updates
+	 *
+	 * @return boolean
+	 */
+	protected function lock() {
+		return $this->getLocker()->acquireExclusiveLock();
+	}
+
+	/**
+	 * Release an obtained lock
+	 *
+	 * @return boolean
+	 */
+	protected function release() {
+		return $this->getLocker()->release();
+	}
+
+	/**
+	 * Returns TRUE if the queue is currently write-locked
+	 *
+	 * @return boolean
+	 */
+	protected function isLocked() {
+		return $this->getLocker()->isLocked();
+	}
 
 }
--- Tests/Unit/Queue/SimpleQueueTest.php
+++ Tests/Unit/Queue/SimpleQueueTest.php
@@ -1,8 +1,215 @@
 <?php
 namespace Dkd\CmisService\Queue;
 
+use Dkd\CmisService\Factory\WorkerFactory;
+use Dkd\CmisService\Tests\Fixtures\Queue\DummyWorker;
+use TYPO3\CMS\Core\Core\Bootstrap;
+use TYPO3\CMS\Core\Locking\Locker;
 use TYPO3\CMS\Core\Tests\UnitTestCase;
 
+/**
+ * Class SimpleQueueTest
+ *
+ * @package Dkd\CmisService\Queue
+ */
 class SimpleQueueTest extends UnitTestCase {
+
+	/**
+	 * Setup
+	 *
+	 * Checks the very first constant defined by the TYPO3 CMS
+	 * initialization "Bootstrap" class. If the constant is not
+	 * defined we assume a minimal environment must be created:
+	 *
+	 * - to define constants some TYPO3 CMS classes need
+	 * - to initialize the TYPO3 CMS class loader _caches_
+	 * - to unregister the actual class loader from TYPO3 CMS,
+	 *   falling back to the Composer autoloading.
+	 *
+	 * The whole thing is initialized by setting PATH_thisScript
+	 * since this constant is the one TYPO3's Bootstrap uses
+	 * when detecting the starting point for other path constants.
+	 *
+	 * @return void
+	 */
+	protected function setUp() {
+		if (FALSE === defined('TYPO3_version')) {
+			define('PATH_thisScript', realpath('vendor/typo3/cms/typo3/index.php'));
+			$bootstrap = Bootstrap::getInstance()->baseSetup('typo3/')->initializeClassLoader()
+				->unregisterClassLoader();
+		}
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['lockingMode'] = Locker::LOCKING_METHOD_DISABLED;
+		parent::setUp();
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function savingCallsSetOnCacheFrontend() {
+		$queue = $this->getMock('Dkd\\CmisService\\Queue\\SimpleQueue', array('lock', 'release'), array(), '', FALSE);
+		$cache = $this->getMock(
+			'Dkd\\CmisService\\Tests\\Fixtures\\Cache\\DummyVariableFrontend',
+			array('set'),
+			array(),
+			'',
+			FALSE
+		);
+		$cache->expects($this->at(0))->method('set')->will($this->returnValue(TRUE));
+		$queue->expects($this->at(0))->method('lock')->will($this->returnValue(TRUE));
+		$queue->expects($this->at(1))->method('release')->will($this->returnValue(TRUE));
+		$queue->setCache($cache);
+		$queue->save();
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function loadCallsExpectedMethodSequence() {
+		$queue = $this->getMock('Dkd\\CmisService\\Queue\\SimpleQueue', array('lock', 'release'), array(), '', FALSE);
+		$cache = $this->getMock(
+			'Dkd\\CmisService\\Tests\\Fixtures\\Cache\\DummyVariableFrontend',
+			array('has', 'get'),
+			array(),
+			'',
+			FALSE
+		);
+		$cache->expects($this->at(0))->method('has')->with(SimpleQueue::CACHE_IDENTITY)->will($this->returnValue(TRUE));
+		$cache->expects($this->at(1))->method('get')->with(SimpleQueue::CACHE_IDENTITY)->will($this->returnValue(array()));
+		$queue->setCache($cache);
+		$queue->load();
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function pickReturnsEarlyNullIfQueueIsEmpty() {
+		$queue = $this->getMock('Dkd\\CmisService\\Queue\\SimpleQueue', array('lock'), array(), '', FALSE);
+		$queue->expects($this->never())->method('lock');
+		$picked = $queue->pick();
+		$this->assertNull($picked);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function pickReturnsTaskAndExecutesExpectedMethodSequenceIfQueueNotEmpty() {
+		$queue = $this->getAccessibleMock('Dkd\\CmisService\\Queue\\SimpleQueue', array('lock', 'save', 'release'), array(), '', FALSE);
+		$task = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Task\\DummyTask', array('assign'));
+		$task->expects($this->at(0))->method('assign');
+		$queue->expects($this->at(0))->method('lock')->will($this->returnValue(TRUE));
+		$queue->expects($this->at(1))->method('save')->will($this->returnValue(TRUE));
+		$queue->expects($this->at(2))->method('release')->will($this->returnValue(TRUE));
+		$queue->_set('queue', array(
+			'dummy-task' => $task
+		));
+		$picked = $queue->pick();
+		$this->assertSame($picked, $task);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function addCallsExpectedMethodSequence() {
+		$queue = $this->getMock('Dkd\\CmisService\\Queue\\SimpleQueue', array('lock', 'save', 'release'), array(), '', FALSE);
+		$task = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Task\\DummyTask', array('getId', 'queue'));
+		$task->expects($this->at(0))->method('getId')->will($this->returnValue('dummy-task'));
+		$task->expects($this->at(1))->method('queue');
+		$queue->expects($this->at(0))->method('lock')->will($this->returnValue(TRUE));
+		$queue->expects($this->at(1))->method('save')->will($this->returnValue(TRUE));
+		$queue->expects($this->at(2))->method('release')->will($this->returnValue(TRUE));
+		$queue->add($task);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function addAllCallsExpectedMethodSequence() {
+		$queue = $this->getMock('Dkd\\CmisService\\Queue\\SimpleQueue', array('lock', 'save', 'release'), array(), '', FALSE);
+		$task1 = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Task\\DummyTask', array('getId', 'queue'));
+		$task1->expects($this->at(0))->method('getId')->will($this->returnValue('dummy-task-1'));
+		$task1->expects($this->at(1))->method('queue');
+		$task2 = $this->getMock('Dkd\\CmisService\\Tests\\Fixtures\\Task\\DummyTask', array('getId', 'queue'));
+		$task2->expects($this->at(0))->method('getId')->will($this->returnValue('dummy-task-2'));
+		$task2->expects($this->at(1))->method('queue');
+		$tasks = array($task1, $task2);
+		$queue->expects($this->at(0))->method('lock')->will($this->returnValue(TRUE));
+		$queue->expects($this->at(1))->method('save')->will($this->returnValue(TRUE));
+		$queue->expects($this->at(2))->method('release')->will($this->returnValue(TRUE));
+		$queue->addAll($tasks);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function lockMethodDelegatesToLocker() {
+		$locker = $this->getMock('TYPO3\\CMS\\Core\\Locking\\Locker', array('acquireExclusiveLock'), array(), '', FALSE);
+		$locker->expects($this->once())->method('acquireExclusiveLock')->will($this->returnValue(TRUE));
+		$queue = $this->getMock('Dkd\\CmisService\\Queue\\SimpleQueue', array('getLocker'), array(), '', FALSE);
+		$queue->expects($this->once())->method('getLocker')->will($this->returnValue($locker));
+		$this->callInaccessibleMethod($queue, 'lock');
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function releaseMethodDelegatesToLocker() {
+		$locker = $this->getMock('TYPO3\\CMS\\Core\\Locking\\Locker', array('release'), array(), '', FALSE);
+		$locker->expects($this->once())->method('release');
+		$queue = $this->getMock('Dkd\\CmisService\\Queue\\SimpleQueue', array('getLocker'), array(), '', FALSE);
+		$queue->expects($this->once())->method('getLocker')->will($this->returnValue($locker));
+		$this->callInaccessibleMethod($queue, 'release');
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function isLockedMethodDelegatesToLocker() {
+		$locker = $this->getMock('TYPO3\\CMS\\Core\\Locking\\Locker', array('isLocked'), array(), '', FALSE);
+		$locker->expects($this->once())->method('isLocked');
+		$queue = $this->getMock('Dkd\\CmisService\\Queue\\SimpleQueue', array('getLocker'), array(), '', FALSE);
+		$queue->expects($this->once())->method('getLocker')->will($this->returnValue($locker));
+		$this->callInaccessibleMethod($queue, 'isLocked');
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function getLockerSetsInternalPropertyAndReturnsInstance() {
+		$queue = $this->getMock('Dkd\\CmisService\\Queue\\SimpleQueue', array(), array(), '', FALSE);
+		$this->assertAttributeEquals(NULL, 'locker', $queue);
+		$locker = $this->callInaccessibleMethod($queue, 'getLocker');
+		$this->assertAttributeSame($locker, 'locker', $queue);
+		$this->assertInstanceOf('TYPO3\\CMS\\Core\\Locking\\Locker', $locker);
+	}
 
 }
