1fb45fe0b8387c0e346d18194f0846528f6318ff
Claus Due <claus@namelesscoder.net>
2014-09-16 14:04:41 +0200

    [FEATURE] Implement indexable table detector
    Summary:
    Second stage "can table be indexed?" detection: first step is
    purely "does table exist in TCA?" and this step adds the "is
    table also enabled in configurations?" step.
    
    Reviewers: dkd-egerer, #forgetit-dev
    
    Reviewed By: dkd-egerer, #forgetit-dev
    
    Differential Revision: https://phabricator.dkd.de/D241

--- Classes/Analysis/Detection/IndexableTableDetector.php
+++ Classes/Analysis/Detection/IndexableTableDetector.php
@@ -1,6 +1,73 @@
 <?php
 namespace Dkd\CmisService\Analysis\Detection;
 
+use Dkd\CmisService\Configuration\Definitions\MasterConfiguration;
+use Dkd\CmisService\Factory\ObjectFactory;
+
+/**
+ * Class IndexableTableDetector
+ */
 class IndexableTableDetector extends AbstractDetector implements DetectorInterface {
+
+	/**
+	 * Gets the names of all tables which are included in
+	 * configuration but not explicitly disabled by it.
+	 *
+	 * @return array
+	 */
+	public function getEnabledTableNames() {
+		$configuration = $this->getConfiguration();
+		$tableConfiguration = $configuration->getTableConfiguration();
+		$definitions = $tableConfiguration->getDefinitions();
+		$tableNames = array();
+		foreach ($definitions as $tableName => $tableDefinition) {
+			$enabled = $this->isTableEnabled($tableName, $tableDefinition);
+			if (TRUE === $enabled) {
+				$tableNames[] = $tableName;
+			}
+		}
+		return $tableNames;
+	}
+
+	/**
+	 * Asserts if table exists according to definition but
+	 * is not explicitly disabled by it. If the $definition
+	 * argument is provided, only it is used when making
+	 * the judgement, otherwise this definition is loaded
+	 * from the master configuration's table definitions.
+	 *
+	 * @param string $table
+	 * @param array|NULL $definition
+	 * @return boolean
+	 */
+	public function isTableEnabled($table, $definition = NULL) {
+		if (NULL === $definition) {
+			$configuration = $this->getConfiguration();
+			$tableConfiguration = $configuration->getTableConfiguration();
+			$definition = $tableConfiguration->get($table);
+		}
+		if (FALSE === is_array($definition)) {
+			$definition = array('enabled' => (boolean) $definition);
+		}
+		return (FALSE === isset($definition['enabled']) || TRUE === (boolean) $definition['enabled']);
+	}
+
+	/**
+	 * Gets the currently active MasterConfiguration
+	 *
+	 * @return MasterConfiguration
+	 */
+	protected function getConfiguration() {
+		return $this->getObjectFactory()->getConfiguration();
+	}
+
+	/**
+	 * Gets the ObjectFactory used by the system.
+	 *
+	 * @return ObjectFactory
+	 */
+	protected function getObjectFactory() {
+		return new ObjectFactory();
+	}
 
 }
--- Tests/Unit/Analysis/Detection/IndexableTableDetectorTest.php
+++ Tests/Unit/Analysis/Detection/IndexableTableDetectorTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace Dkd\CmisService\Tests\Unit\Analysis\Detection;
 
+use Dkd\CmisService\Analysis\Detection\IndexableTableDetector;
+use Dkd\CmisService\Tests\Fixtures\Configuration\DummyMasterConfiguration;
 use TYPO3\CMS\Core\Tests\UnitTestCase;
 
 /**
@@ -9,13 +11,146 @@ use TYPO3\CMS\Core\Tests\UnitTestCase;
 class IndexableTableDetectorTest extends UnitTestCase {
 
 	/**
-	 * Placeholder
+	 * Unit test
 	 *
 	 * @test
 	 * @return void
 	 */
-	public function placeholder() {
-		$this->markTestIncomplete('Tests not yet implemented');
+	public function getObjectFactoryReturnsObjectFactory() {
+		$instance = new IndexableTableDetector();
+		$result = $this->callInaccessibleMethod($instance, 'getObjectFactory');
+		$this->assertInstanceOf('Dkd\\CmisService\\Factory\\ObjectFactory', $result);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function getConfigurationReturnsObjectFactoryMasterConfiguration() {
+		$dummyConfiguration = new DummyMasterConfiguration();
+		$objectFactory = $this->getMock('Dkd\\CmisService\\Factory\\ObjectFactory', array('getConfiguration'));
+		$objectFactory->expects($this->once())->method('getConfiguration')->will($this->returnValue($dummyConfiguration));
+		$instance = $this->getMock('Dkd\\CmisService\\Analysis\\Detection\\IndexableTableDetector', array('getObjectFactory'));
+		$instance->expects($this->once())->method('getObjectFactory')->will($this->returnValue($objectFactory));
+		$result = $this->callInaccessibleMethod($instance, 'getConfiguration');
+		$this->assertSame($dummyConfiguration, $result);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function getEnabledTableNamesCallsExpectedMethodSequenceAndReturnsExpectedTableNames() {
+		$tableConfiguration = $this->getMock(
+			'Dkd\\CmisService\\Configuration\\Definitions\\TableConfiguration',
+			array('getDefinitions')
+		);
+		$tableConfiguration->expects($this->once())->method('getDefinitions')->will($this->returnValue(array(
+			'tt_content' => array('enabled' => '1'),
+			'pages' => array('enabled' => '0')
+		)));
+		$masterConfiguration = $this->getMock(
+			'Dkd\\CmisService\\Configuration\\Definitions\\MasterConfiguration',
+			array('getTableConfiguration'),
+			array(),
+			'',
+			FALSE
+		);
+		$masterConfiguration->expects($this->once())->method('getTableConfiguration')
+			->will($this->returnValue($tableConfiguration));
+		$objectFactory = $this->getMock('Dkd\\CmisService\\Factory\\ObjectFactory', array('getConfiguration'));
+		$objectFactory->expects($this->once())->method('getConfiguration')->will($this->returnValue($masterConfiguration));
+		$instance = $this->getMock(
+			'Dkd\\CmisService\\Analysis\\Detection\\IndexableTableDetector',
+			array('getObjectFactory', 'isTableEnabled')
+		);
+		$instance->expects($this->at(0))->method('getObjectFactory')->will($this->returnValue($objectFactory));
+		$instance->expects($this->at(1))->method('isTableEnabled')->with('tt_content')->will($this->returnValue(TRUE));
+		$instance->expects($this->at(2))->method('isTableEnabled')->with('pages')->will($this->returnValue(FALSE));
+		$result = $instance->getEnabledTableNames();
+		$this->assertEquals(array('tt_content'), $result);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function isTableEnabledWithoutDefinitionArgumentFetchesDefinitionFromConfiguration() {
+		$tableConfiguration = $this->getMock('Dkd\\CmisService\\Configuration\\Definitions\\TableConfiguration', array('get'));
+		$tableConfiguration->expects($this->once())->method('get')->with('foobar')->will($this->returnValue(array()));
+		$masterConfiguration = $this->getMock(
+			'Dkd\\CmisService\\Configuration\\Definitions\\MasterConfiguration',
+			array('getTableConfiguration'),
+			array(),
+			'',
+			FALSE
+		);
+		$masterConfiguration->expects($this->once())->method('getTableConfiguration')
+			->will($this->returnValue($tableConfiguration));
+		$objectFactory = $this->getMock('Dkd\\CmisService\\Factory\\ObjectFactory', array('getConfiguration'));
+		$objectFactory->expects($this->once())->method('getConfiguration')->will($this->returnValue($masterConfiguration));
+		$instance = $this->getMock('Dkd\\CmisService\\Analysis\\Detection\\IndexableTableDetector', array('getObjectFactory'));
+		$instance->expects($this->once())->method('getObjectFactory')->will($this->returnValue($objectFactory));
+		$result = $instance->isTableEnabled('foobar');
+		$this->assertTrue($result);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function isTableEnabledWithDefinitionDoesNotFetchDefinitionFromConfiguration() {
+		$instance = $this->getMock('Dkd\\CmisService\\Analysis\\Detection\\IndexableTableDetector', array('getObjectFactory'));
+		$instance->expects($this->never())->method('getObjectFactory');
+		$result = $instance->isTableEnabled('foobar', array());
+		$this->assertTrue($result);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function isTableEnabledReturnsFalseIfDefinitionDisablesIt() {
+		$instance = $this->getMock('Dkd\\CmisService\\Analysis\\Detection\\IndexableTableDetector', array('getObjectFactory'));
+		$instance->expects($this->never())->method('getObjectFactory');
+		$result = $instance->isTableEnabled('foobar', array('enabled' => 0));
+		$this->assertFalse($result);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function isTableEnabledReturnsBooleanValueOfDefinitionIfDefinitionIsNotArrayAndEvaluatesToTrue() {
+		$instance = $this->getMock('Dkd\\CmisService\\Analysis\\Detection\\IndexableTableDetector', array('getObjectFactory'));
+		$instance->expects($this->never())->method('getObjectFactory');
+		$result = $instance->isTableEnabled('foobar', '1');
+		$this->assertTrue($result);
+	}
+
+	/**
+	 * Unit test
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function isTableEnabledReturnsBooleanValueOfDefinitionIfDefinitionIsNotArrayAndEvaluatesToFalse() {
+		$instance = $this->getMock('Dkd\\CmisService\\Analysis\\Detection\\IndexableTableDetector', array('getObjectFactory'));
+		$instance->expects($this->never())->method('getObjectFactory');
+		$result = $instance->isTableEnabled('foobar', '0');
+		$this->assertFalse($result);
 	}
 
 }
