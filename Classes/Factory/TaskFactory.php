a1a94215bce0966563e504586781030ed264afaa
Claus Due <claus@namelesscoder.net>
2014-09-16 13:47:34 +0200

    [TASK] Fill in TaskFactory methods
    Reviewers: dkd-egerer, #forgetit-dev
    
    Reviewed By: dkd-egerer, #forgetit-dev
    
    Differential Revision: https://phabricator.dkd.de/D237

--- Classes/Factory/TaskFactory.php
+++ Classes/Factory/TaskFactory.php
@@ -1,6 +1,41 @@
 <?php
 namespace Dkd\CmisService\Factory;
+use Dkd\CmisService\Task\EvictionTask;
+use Dkd\CmisService\Task\RecordIndexTask;
 
+/**
+ * Class TaskFactory
+ */
 class TaskFactory {
+
+	/**
+	 * Creates a Task to index one record from a table.
+	 *
+	 * @param string $table
+	 * @param integer $uid
+	 * @param array|NULL fields
+	 * @return RecordIndexTask
+	 */
+	public function createRecordIndexingTask($table, $uid, $fields = NULL) {
+		$task = new RecordIndexTask();
+		$task->setParameter(RecordIndexTask::OPTION_TABLE, $table);
+		$task->setParameter(RecordIndexTask::OPTION_UID, $uid);
+		$task->setParameter(RecordIndexTask::OPTION_FIELDS, $fields);
+		return $task;
+	}
+
+	/**
+	 * Creates a Task to evict one or all record(s) from the index.
+	 *
+	 * @param string $table
+	 * @param integer|NULL $uid
+	 * @return EvictionTask
+	 */
+	public function createEvictionTask($table, $uid = NULL) {
+		$task = new EvictionTask();
+		$task->setParameter(EvictionTask::OPTION_TABLE, $table);
+		$task->setParameter(EvictionTask::OPTION_UID, $uid);
+		return $task;
+	}
 
 }
--- Tests/Unit/Factory/TaskFactoryTest.php
+++ Tests/Unit/Factory/TaskFactoryTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace Dkd\CmisService\Tests\Unit\Factory;
 
+use Dkd\CmisService\Factory\TaskFactory;
+use Dkd\CmisService\Task\RecordIndexTask;
 use TYPO3\CMS\Core\Tests\UnitTestCase;
 
 /**
@@ -9,13 +11,18 @@ use TYPO3\CMS\Core\Tests\UnitTestCase;
 class TaskFactoryTest extends UnitTestCase {
 
 	/**
-	 * Placeholder
+	 * Unit test
 	 *
 	 * @test
 	 * @return void
 	 */
-	public function placeholder() {
-		$this->markTestIncomplete('Tests not yet implemented');
+	public function createsCmisIndexTasks() {
+		$factory = new TaskFactory();
+		$task = $factory->createRecordIndexingTask('tt_content', 123, array('uid'));
+		$this->assertInstanceOf('Dkd\\CmisService\\Task\\RecordIndexTask', $task);
+		$this->assertEquals('tt_content', $task->getParameter(RecordIndexTask::OPTION_TABLE));
+		$this->assertEquals(123, $task->getParameter(RecordIndexTask::OPTION_UID));
+		$this->assertEquals(array('uid'), $task->getParameter(RecordIndexTask::OPTION_FIELDS));
 	}
 
 }
