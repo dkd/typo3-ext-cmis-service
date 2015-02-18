5d21366172e7197c646220d8a277eed00d339b08
Claus Due <claus@namelesscoder.net>
2015-02-18 17:42:19 +0100

    [TASK] Add basic CMIS session parameters to CmisConfiguration
    Summary:
    This change adds CMIS session initialisation parameters
    to the CMIS configuration object and a public function
    to retrieve an array of session parameters using the
    names from `dkd/php-cmis`.
    
    Reviewers: #forgetit-dev, dkd-egerer
    
    Reviewed By: #forgetit-dev, dkd-egerer
    
    Differential Revision: https://phabricator.dkd.de/D836

--- Classes/Configuration/Definitions/CmisConfiguration.php
+++ Classes/Configuration/Definitions/CmisConfiguration.php
@@ -1,20 +1,38 @@
 <?php
 namespace Dkd\CmisService\Configuration\Definitions;
 
+use Dkd\PhpCmis\Enum\BindingType;
+use Dkd\PhpCmis\SessionParameter;
+
 /**
  * CMIS Configuration Definition
  */
 class CmisConfiguration extends AbstractConfigurationDefinition implements ConfigurationDefinitionInterface {
 
-	const HOSTNAME = 'hostname';
-	const PORT = 'port';
+	const ID = 'id';
+	const URL = 'url';
+	const USERNAME = 'username';
+	const PASSWORD = 'password';
+	const BINDINGTYPE = 'bindingType';
 
 	/**
 	 * @var array
 	 */
 	protected $defaults = array(
-		self::HOSTNAME => 'localhost',
-		self::PORT => 8080
+		self::BINDINGTYPE => BindingType::BROWSER
 	);
+
+	/**
+	 * @return array
+	 */
+	public function getSessionParameters() {
+		return array(
+			SessionParameter::BROWSER_URL => $this->get(self::URL),
+			SessionParameter::BINDING_TYPE => $this->get(self::BINDINGTYPE),
+			SessionParameter::REPOSITORY_ID => $this->get(self::ID),
+			SessionParameter::USER => $this->get(self::USERNAME),
+			SessionParameter::PASSWORD => $this->get(self::PASSWORD)
+		);
+	}
 
 }
--- Tests/Unit/Configuration/Definitions/CmisConfigurationTest.php
+++ Tests/Unit/Configuration/Definitions/CmisConfigurationTest.php
@@ -2,6 +2,7 @@
 namespace Dkd\CmisService\Tests\Unit\Configuration\Definitions;
 
 use Dkd\CmisService\Configuration\Definitions\CmisConfiguration;
+use Dkd\PhpCmis\SessionParameter;
 use TYPO3\CMS\Core\Tests\UnitTestCase;
 
 /**
@@ -10,31 +11,66 @@ use TYPO3\CMS\Core\Tests\UnitTestCase;
 class CmisConfigurationTest extends UnitTestCase {
 
 	/**
-	 * Unit test
-	 *
-	 * @test
+	 * @dataProvider getSettingTestValues
+	 * @param string $name
+	 * @param mixed $value
 	 * @return void
 	 */
-	public function supportsHostnameSetting() {
+	public function testGetSettingReturnsDefinedValue($name, $value) {
 		$instance = new CmisConfiguration();
 		$instance->setDefinitions(array(
-			CmisConfiguration::HOSTNAME => 'foobar'
+			$name => $value
 		));
-		$this->assertEquals('foobar', $instance->get(CmisConfiguration::HOSTNAME));
+		$this->assertEquals($value, $instance->get($name));
 	}
 
 	/**
-	 * Unit test
-	 *
-	 * @test
+	 * @return array
+	 */
+	public function getSettingTestValues() {
+		return array(
+			array(CmisConfiguration::URL, 'localhost'),
+			array(CmisConfiguration::ID, 'id'),
+			array(CmisConfiguration::USERNAME, 'username'),
+			array(CmisConfiguration::PASSWORD, 'password')
+		);
+	}
+
+	/**
+	 * @dataProvider getSessionParametersTestValues
+	 * @param array $definitions
+	 * @param array $expected
 	 * @return void
 	 */
-	public function supportsPortSetting() {
+	public function testGetSessionParametersReturnsDefinedSessionParameters(array $definitions, array $expected) {
 		$instance = new CmisConfiguration();
-		$instance->setDefinitions(array(
-			CmisConfiguration::PORT => 'foobar'
-		));
-		$this->assertEquals('foobar', $instance->get(CmisConfiguration::PORT));
+		$instance->setDefinitions($definitions);
+		$result = $instance->getSessionParameters();
+		$this->assertEquals($expected, $result);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getSessionParametersTestValues() {
+		return array(
+			array(
+				array(
+					CmisConfiguration::URL => 'url',
+					CmisConfiguration::ID => 'id',
+					CmisConfiguration::USERNAME => 'username',
+					CmisConfiguration::PASSWORD => 'password',
+					CmisConfiguration::BINDINGTYPE => 'binding'
+				),
+				array(
+					SessionParameter::BROWSER_URL => 'url',
+					SessionParameter::BINDING_TYPE => 'binding',
+					SessionParameter::REPOSITORY_ID => 'id',
+					SessionParameter::USER => 'username',
+					SessionParameter::PASSWORD => 'password'
+				)
+			)
+		);
 	}
 
 }
