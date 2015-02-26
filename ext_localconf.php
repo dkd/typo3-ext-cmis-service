50de0a09e46af779d97c7e96d2376f260a59b34d
Claus Due <claus@namelesscoder.net>
2015-02-26 16:13:18 +0100

    [TASK] Add necessary registrations for TYPO3 integration
    Reviewers: dkd-egerer, #forgetit-dev
    
    Reviewed By: dkd-egerer, #forgetit-dev
    
    Differential Revision: https://phabricator.dkd.de/D924

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,8 @@
 <?php
 
+// TEMPORARY MEASURE: use composer autoloader until we are shipping w/ libs
+// @TODO: remove when release mechanism is built
+require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('cmis_service', 'vendor/autoload.php');
 
 // caches definitions:
 // Queue - assigned to "system" group to avoid clearing cached states on anything but admin access.
@@ -18,3 +21,10 @@ unset($caches);
 // add our CommandController which handles cronjobs
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][]
 	= 'Dkd\\CmisService\\Command\\CronjobCommandController';
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][]
+	= 'Dkd\\CmisService\\Hook\\DataHandlerListener';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][]
+	= 'Dkd\\CmisService\\Hook\\DataHandlerListener';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['moveRecordClass'][]
+	= 'Dkd\\CmisService\\Hook\\DataHandlerListener';
---
+++ ext_tables.php
@@ -0,0 +1,4 @@
+<?php
+
+// Configuration registration - static TypoScript for this extension
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'CMIS base settings');
