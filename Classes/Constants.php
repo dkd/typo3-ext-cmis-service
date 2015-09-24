bf9a97a511464e987e7eb9032a2246c4f71b6984
Claus Due <claus@namelesscoder.net>
2015-09-24 15:31:53 +0200

    [TASK] Add object type ID of sys_file as constant
    Reviewers: dkd-goslar
    
    Reviewed By: dkd-goslar
    
    Differential Revision: https://phabricator.dkd.de/D1884

--- Classes/Constants.php
+++ Classes/Constants.php
@@ -12,6 +12,7 @@ abstract class Constants {
 	const CMIS_DOCUMENT_TYPE_ARBITRARY = 'D:dkd:typo3:arbitrary';
 	const CMIS_DOCUMENT_TYPE_PAGES = 'F:dkd:typo3:pages';
 	const CMIS_DOCUMENT_TYPE_CONTENT = 'D:dkd:typo3:tt_content';
+	const CMIS_DOCUMENT_TYPE_FILE = 'D:dkd:typo3:sys_file';
 	const CMIS_PROPERTY_TYPO3TABLE = 'dkd:typo3:general:record_table';
 	const CMIS_PROPERTY_TYPO3UID = 'dkd:typo3:general:record_id';
 	const CMIS_PROPERTY_RAWDATA = 'dkd:typo3:general:record_data';
