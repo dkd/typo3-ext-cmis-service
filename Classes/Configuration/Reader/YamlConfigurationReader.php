f24d35bcce82d9bead5e850784204350a5dbbe2a
Johannes Goslar <johannes.goslar@dkd.de>
2017-02-16 19:41:28 +0100

    [BUGFIX] YAML cache reader

--- Classes/Configuration/Reader/YamlConfigurationReader.php
+++ Classes/Configuration/Reader/YamlConfigurationReader.php
@@ -34,7 +34,7 @@ class YamlConfigurationReader implements ConfigurationReaderInterface {
 			throw new \RuntimeException('Configuration definition class "' . $definitionClassName . '" must implement interface ' .
 				'"Dkd\\CmisService\\Configuration\\Definitions\\ConfigurationDefinitionInterface"', 1409923995);
 		}
-		$yamlArray = (array) Yaml::parse($resourceIdentifier);
+		$yamlArray = (array) Yaml::parse(file_get_contents($resourceIdentifier));
 		$definition = new $definitionClassName();
 		$definition->setDefinitions($yamlArray);
 		return $definition;
